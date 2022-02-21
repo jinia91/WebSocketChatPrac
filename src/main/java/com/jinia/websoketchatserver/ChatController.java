6f0f3468c1f71033b48752783f4278040966b8c7
jinia91 <creee77@gmail.com>
2022-02-21 22:31:38 +0900

    SSE 프로토콜로 채팅 구현 완료

---
+++ src/main/java/com/jinia/websoketchatserver/ChatController.java
@@ -0,0 +1,30 @@
+package com.jinia.websoketchatserver;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final ChatRepository chatRepository;
+
+    // SSE 프로토콜(HTTP X)
+    @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> getMessage(@PathVariable String sender, @PathVariable String receiver){
+        return chatRepository.mFindBySender(sender, receiver)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @PostMapping("/chat")
+    public Mono<Chat> insertMessage(@RequestBody Chat chat){
+        chat.setCreatedAt(LocalDateTime.now());
+        return chatRepository.save(chat);
+    }
+}
---
+++ src/main/java/com/jinia/websoketchatserver/ChatRepository.java
@@ -0,0 +1,14 @@
+package com.jinia.websoketchatserver;
+
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+import reactor.core.publisher.Flux;
+
+public interface ChatRepository extends ReactiveMongoRepository<Chat, String > {
+
+    // flux : 데이터 흐름, stream 개념, response를 유지하면서 데이터 계속 흘려보내기
+    @Tailable // 커서 안닫고 유지
+    @Query("{sender: ?0, receiver: ?1}")
+    Flux<Chat> mFindBySender(String sender, String receiver);
+}
