982f019738eaa2fe32f5dbce2082db0b0a926230
jinia91 <creee77@gmail.com>
2022-02-21 21:33:09 +0900

    스프링 리액티브 웹플럭스로 채팅서버 만들기

---
+++ src/main/java/com/jinia/websoketchatserver/WebsoketchatserverApplication.java
@@ -0,0 +1,13 @@
+package com.jinia.websoketchatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsoketchatserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsoketchatserverApplication.class, args);
+	}
+
+}
